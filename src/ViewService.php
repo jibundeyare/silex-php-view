0365c2499dc5bb99b4cbf968e732b8dbc1f89a0a
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-01-24 21:30:29 +0100

    ViewService class extends jibundeyare/raw-php-view package's ViewService class

--- src/ViewService.php
+++ src/ViewService.php
@@ -3,34 +3,23 @@
 namespace SilexPhpView;
 
 use Pimple\Container;
+use RawPhpView\ViewService as RawPhpViewService;
 
-class ViewService
+class ViewService extends RawPhpViewService
 {
     private $app;
-    private $path;
 
     public function __construct(Container $app, $path)
     {
+        parent::__construct($path);
         $this->app = $app;
-        $this->path = $path;
     }
 
     public function render($view, array $parameters = array())
     {
         $parameters['app'] = $this->app;
 
-        return evalTemplate($this->path . '/' . $view, $parameters);
+        return parent::render($view, $parameters);
     }
-
-}
-
-function evalTemplate($file, array $parameters = []) {
-    extract($parameters, EXTR_SKIP);
-    unset($parameters);
-    ob_start();
-    require $file;
-    unset($file);
-
-    return ob_get_clean();
 }
 
